d6fc05c7689518786d2bc2e6077a3654737b4da1
David Stripeik <dpstrip@outlook.com>
2018-08-25 12:02:13 -0500

    lab 18 put user name in

--- MVCWebApplication/Controllers/EmployeeController.cs
+++ MVCWebApplication/Controllers/EmployeeController.cs
@@ -27,6 +27,7 @@ namespace MVCWebApplication.Controllers
         public ActionResult Index()
         {
             EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            employeeListViewModel.UserName = User.Identity.Name; //
             EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
             List<Employee> employees = empBal.GetEmployees();
             List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
--- MVCWebApplication/ViewModels/EmployeeListViewModel.cs
+++ MVCWebApplication/ViewModels/EmployeeListViewModel.cs
@@ -8,6 +8,6 @@ namespace MVCWebApplication.ViewModels
     public class EmployeeListViewModel
     {
         public List<EmployeeViewModel>Employees {get; set;}
-        //public string UserName { get; set; }
+        public string UserName { get; set; }
     }
 }
